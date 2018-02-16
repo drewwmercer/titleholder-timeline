d5cb049c274bf02ed6f37d1da17a47d30a9ca9d3
Drew Mercer <japanexpress@gmail.com>
2018-02-15 20:20:36 -0500

    Adding to the layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Timeline } from '../models/timeline.interface';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  
+export class AppComponent implements OnInit {
+  public timeline: Timeline;
+
+  public ngOnInit(): void {
+    this.timeline = {
+      localTitle: "Miss Clayton's Outstading Teen",
+      stateTitle: "Miss North Carolina's Outstading Teen",
+      introduction: 'intro text goes here',
+      socialMedia: [],
+      titlelog: [],
+      uodates: []
+    };
+  }
 }
